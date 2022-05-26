44692af6dfc3ae258ce6297ef7f84efcf6f62d20
souravit <sourav.singhit@gmail.com>
2022-05-26 15:26:18 -0400

    Group anamgram

---
+++ src/HashMap/GroupAnagram.java
@@ -0,0 +1,37 @@
+package HashMap;
+
+import java.util.*;
+
+public class GroupAnagram {
+    public static void main(String[] args) {
+        String[] str = {"eat","tea","tan","ate","nat","bat"};
+        List<List<String>> value = helper(str);
+        System.out.println(value);
+    }
+
+    private static List<List<String>> helper(String[] strs) {
+        HashMap<String, ArrayList<String>> storeMap = new HashMap<>();
+        List<List<String>> result = new ArrayList<>();
+        for(int i = 0 ; i < strs.length; i++){
+            char[] ch = strs[i].toCharArray();
+            Arrays.sort(ch);
+            String afterSort = String.valueOf(ch);
+
+            if(storeMap.containsKey(afterSort)){
+                //add value to the map
+                storeMap.get(afterSort).add(strs[i]);
+            }else{
+                //create new value in the map
+                //Learn **
+                storeMap.computeIfAbsent(afterSort, x -> new ArrayList<String>()).add(strs[i]);
+            }
+        }
+        for(Map.Entry<String, ArrayList<String>> resultMap : storeMap.entrySet()){
+            result.add(resultMap.getValue());
+        }
+
+
+        return result;
+
+    }
+}
